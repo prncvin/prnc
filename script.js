1d34d9454d40a4fbc9f3f72838e4d91ec1694d43
prince <midnightbssyt@gmail.com>
2025-08-19 10:46:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function initializeVisitorCounter() {
     let totalVisitors = localStorage.getItem('totalVisitorCount');
     if (!totalVisitors) {
-      totalVisitors = 921234;
+      totalVisitors = 5671;
       localStorage.setItem('totalVisitorCount', totalVisitors);
     } else {
       totalVisitors = parseInt(totalVisitors);
